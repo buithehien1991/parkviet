211fe15e031bb2578f9b3da8f9912b37b63a7b9c
Bùi Thế Hiển <buithehien1991@gmail.com>
2019-12-06 20:39:32 +0700

    Thêm trang thông tin cho Supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,10 +1,14 @@
 class Supplier < ApplicationRecord
   belongs_to :user
   belongs_to :store
+  belongs_to :province, optional: true
+  belongs_to :district, optional: true
+  belongs_to :commune, optional: true
+
   validates_presence_of :name
   validates :code, uniqueness: {scope: :store}, :allow_blank => true
 
-  enum status: [:active, :inactive, :archived]
+  enum status: {active: 0, inactive: 1, archived: 2}
 
   # update_index('suppliers#supplier', :self)
   has_paper_trail(meta: { store_id: :store_id })
